78700cc9ad10d48765e4ee8b13345d33bce83aae
shirley <shirley20041028@gmail.com>
2025-05-12 03:18:25 +0800

    設定後台功能

--- Test/urls.py
+++ Test/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path ,include
 from app01 import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app01/', include('app01.urls')),
